6e78e268ced2b7d752eb56e2ca5a1704060af454
aitageo <aitageo1980@hotmail.com>
2024-09-04 18:11:11 -0500

    empezando el carrito

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const titulo = document.getElementById('titulo');
 
 titulo.addEventListener('mouseout',()=>{
     titulo.classList.remove('animate__bounceIn');
-    void titulo.offsetWidth; 
+    // void titulo.offsetWidth; 
     titulo.classList.add('animate__bounceIn');
     // console.log('por aqui paso');
     
@@ -42,16 +42,80 @@ titulo.addEventListener('mouseout',()=>{
 // })
 
 
-const button = document.querySelectorAll('.btn');
-console.log(button);
-button.addEventListener('click',(e)=>{
-     e.preventDefault();
-     console.log("Diste click en agregar");
-     
-})
+// const buttons = document.querySelectorAll('.btn');
+// console.log(buttons);
+
+// buttons.forEach(button=>{
+//    button.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     console.log('Diste click');
+    
+//    })
+// });
 
 
 
+const card = document.querySelector('.card');
+const card_body = document.querySelector('.card-body');
+const  agregar = document.getElementById('agregar');
+
+    agregar.addEventListener('click',(e)=>{
+        console.log(e.target);
+       e.stopPropagation();
+       console.log('diste click en agregar');
+       
+    })
+    
+    card_body.addEventListener('click',(e)=>{
+        e.stopPropagation();
+        console.log(e.target);
+        
+        console.log('diste click en card_body');
+        
+    })
+    
+    card.addEventListener('click',(e)=>{
+        console.log(e.target);
+        
+        e.stopPropagation();
+        console.log('diste click en card');
+    
+});
+
+
+const productos = [{
+    titulo : "",
+    imagen : "",
+    precio : "",
+}]
+
+
+card.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('.card-text')){
+    console.log('diste click en card');
+    }
+    if (e.target.classList.contains('card-img-top')){
+        console.log('diste click en la imagen');  
+    }
+    if (e.target.classList.contains('card-tittle')){
+        console.log('diste click en la imagen');  
+    }
+    
+});
+
+
+
+function mostrarCarrito(){
+  const carrito = document.querySelector('.titulo img')
+  carrito.addEventListener('click',()=>{
+    carrito.style.display = "block";
+    console.log("diste click en el carrito");
+    
+  })
+}
+
+
+mostrarCarrito()
 
 
 
