e89a2ecc753e778d089c6c3f1800ed24fec96cc7
aitageo <aitageo1980@hotmail.com>
2024-09-03 19:45:56 -0500

    trabajando en los eventos del sitio

--- main.js
+++ main.js
@@ -6,17 +6,61 @@ titulo.addEventListener('mouseout',()=>{
     titulo.classList.remove('animate__bounceIn');
     void titulo.offsetWidth; 
     titulo.classList.add('animate__bounceIn');
-    console.log('por aqui paso');
+    // console.log('por aqui paso');
     
 });
 
 
+// const card = document.querySelector('.card');
+// console.log(card.children);
+// console.log(card.parentElement);
+// console.log(card.nextSibling);
 
 
-const agregar = document.getElementById('agregar');
 
-agregar.addEventListener('click',()=>{
-    alert("Agregando al carrito");
+// const navegacion = document.querySelector('.navegacion');
+
+// console.log(navegacion.children[0].children[0].children[0].children[0]);
+// // navegacion.removeChild(navegacion.children[0]);
+
+// const nav = document.querySelector('.navegacion .nav ul');
+// const nuevoLi = document.createElement('LI');
+// const nuevoA = document.createElement('A');
+// console.log(nav);
+// nuevoA.innerHTML = "Productos"
+
+// nav.appendChild(nuevoLi);
+// nuevoLi.appendChild(nuevoA);
+
+
+
+// const button = document.getElementById('button')
+// button.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     alert("Diste click");
+
+// })
+
+
+const button = document.querySelectorAll('.btn');
+console.log(button);
+button.addEventListener('click',(e)=>{
+     e.preventDefault();
+     console.log("Diste click en agregar");
+     
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
