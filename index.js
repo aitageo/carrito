05401c1ab6f7f55a47db760aa64e97fedb35786e
aitageo <aitageo1980@hotmail.com>
2024-09-13 19:21:20 -0500

    mejorando el carrito de compras

--- index.js
+++ index.js
@@ -26,6 +26,20 @@ function cargarProductos(e) {
     if (e.target.classList.contains('btn-primary')) {
         const productoSeleccionado = e.target.parentElement.parentElement;
         leerDatos(productoSeleccionado);
+
+
+      const button = e.target
+      button.innerHTML = "Agregando al carrito"
+
+      setTimeout(()=>{
+       button.innerHTML = "Agregado al carrito"
+      },1000)
+
+      setTimeout(()=>{
+        button.innerHTML = "Agregar al carrito"
+       },3000)
+      
+
     }
 }
 
@@ -66,48 +80,21 @@ function actualizarCarritoHtml() {
             <td><img src="${img}" width="50"></td>
             <td>${nombre}</td>
             <td>${precio}</td>
-            <td><input type="number" class="form-control ActualizarCantidad" value="${cantidad}" data-id="${id}"></td>
+            <td><input type="number" value="${cantidad}" class="form-control  ActualizarCantidad" data-id="${id}"> </td>
             <td><a href="#" class="btn btn-danger eliminarProducto" data-id="${id}">X</a></td>
         `;
 
         contenedorCarrito.appendChild(row);
         const eliminar = row.querySelector('.eliminarProducto');
         eliminar.addEventListener('click',eliminarProducto);
+    
 
         const ActualizarCantidadInput = row.querySelector('.ActualizarCantidad');
         ActualizarCantidadInput.addEventListener('input',ActualizarCantidad);
         
     });
+    //llamada funcion total 
     total()
-}
-
-
-function eliminarProducto(e){
-    const id =  e.target.getAttribute('data-id');
-    console.log(`eliminando...  ${id}`);
-    console.log(totalCarrito);
-    
-    totalCarrito = totalCarrito.map(producto=>{
-        if(producto.id === id){
-          if (producto.cantidad > 1) {
-              producto.cantidad--
-          }
-          else {
-            return null
-          }
-        }
-        
-        return producto
-    })
-    totalCarrito = totalCarrito.filter(producto=>producto !== null)
-    actualizarCarritoHtml()
-}
-
-
-function total(){
-   const totalProductos = totalCarrito.reduce((total,producto)=> total + (producto.cantidad * producto.precio),0);
-   const totalCarritoHtml = document.getElementById('total-carrito');
-   totalCarritoHtml.innerHTML =  `Total $${Math.floor(totalProductos)}`
 }
 
 
@@ -116,14 +103,46 @@ function ActualizarCantidad(e){
     const id =  e.target.getAttribute('data-id');
     const NuevaCantidad = parseInt(e.target.value);
 
-    totalCarrito = totalCarrito.map(producto=>{
-        if(producto.id === id){
-           producto.cantidad = NuevaCantidad;
+    totalCarrito = totalCarrito.map((producto)=>{
+        if (producto.id === id){
+            producto.cantidad = NuevaCantidad;
         }
         return producto
     })
-
     total()
-    actualizarCarritoHtml()
+    actualizarCarritoHtml();
 }
+
+
+function eliminarProducto(e){
+   const id = e.target.getAttribute('data-id');
+     totalCarrito = totalCarrito.map(producto=>{  // verifica si el id que se obtiene es igual al id del producto
+       if(producto.id ===id){
+        if (producto.cantidad >1 ) {// si es mayor a 1 disminuye sino devuelve null que es igual a nada
+            producto.cantidad--;
+        } else {
+            return null;
+        }
+       }
+       return producto
+   })
+   
+   totalCarrito = totalCarrito.filter(producto=> producto !== null);
+   console.log(totalCarrito);
+    actualizarCarritoHtml();
+}
+
+
+function total(){
+  const totalProductos = totalCarrito.reduce((total,producto)=> total + (producto.cantidad * producto.precio),0);
+  const totalCarritoHtml = document.getElementById('total-carrito');
+  totalCarritoHtml.innerHTML = `Total $${Math.floor(totalProductos)}`    
+   
+}
+
+
+
+
+
+
 
